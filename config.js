263887e958bba4e2d8b52e2127c52a2253a2b43d
Richard Hurt <rnhurt@gmail.com>
2017-03-05 16:00:26 -0500

    Update config.js to use environment variables

--- config.js
+++ config.js
@@ -1,27 +1,32 @@
 module.exports = {
 
-  kmsEncryptedHookUrl: "<kmsEncryptedHookUrl>", // encrypted slack webhook url
-  unencryptedHookUrl: "<unencryptedHookUrl>", // unencrypted slack webhook url
-  slackChannel: "#general",  // slack channel to send a message to
-  slackUsername: "AWS SNS via Lamda", // slack username to user for messages
-  icon_emoji: ":robot_face:", // slack emoji icon to use for messages
-  orgIcon: "", // url to icon for your organization for display in the footer of messages
-  orgName: "", // name of your organization for display in the footer of messages
+  kmsEncryptedHookUrl: process.env.kmsEncryptedHookUrl, // encrypted slack webhook url
+  unencryptedHookUrl: process.env.unencryptedHookUrl,   // unencrypted slack webhook url
+  slackChannel: process.env.slackChannel,      // slack channel to send a message to
+  slackUsername: process.env.slackUsername,    // slack username to user for messages
+  icon_emoji: process.env.icon_emoji,          // slack emoji icon to use for messages
+  orgIcon: process.env.orgIcon,                // url to icon for your organization for display in the footer of messages
+  orgName: process.env.orgName,                // name of your organization for display in the footer of messages
   services: {
     elasticbeanstalk: {
-      match_text: "ElasticBeanstalkNotifications" // text in the sns message or topicname to match on to process this service type
+      // text in the sns message or topicname to match on to process this service type
+      match_text: "ElasticBeanstalkNotifications"
     },
     cloudwatch: {
-      match_text: "CloudWatchNotifications" // text in the sns message or topicname to match on to process this service type
+      // text in the sns message or topicname to match on to process this service type
+      match_text: "CloudWatchNotifications"
     },
     codedeploy: {
-      match_text: "CodeDeploy" // text in the sns message or topicname to match on to process this service type
+      // text in the sns message or topicname to match on to process this service type
+      match_text: "CodeDeploy"
     },
     elasticache: {
-      match_text: "ElastiCache" // text in the sns message or topicname to match on to process this service type
+      // text in the sns message or topicname to match on to process this service type
+      match_text: "ElastiCache"
     },
     autoscaling: {
-      match_text: "AutoScaling" // text in the sns message or topicname to match on to process this service type
+      // text in the sns message or topicname to match on to process this service type
+      match_text: "AutoScaling"
     }
   }
 
