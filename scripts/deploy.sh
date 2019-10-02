77231d03ee6d3b2da2629390ec23091adcb639a1
Alejandro Pérez <alejandro.perez.torres@gmail.com>
2019-10-02 15:48:56 -0500

    modify deploy script to include slack_channel enviroment variable

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,5 @@ NODE_LAMBDA=./node_modules/node-lambda/bin/node-lambda
 test -s $NODE_LAMBDA || { echo "node-lambda not installed. Run 'npm install' first."; exit 1; }
 mkdir -p tmp
 cat .env | grep HOOK_URL > ./tmp/deploy.env
+cat .env | grep SLACK_CHANNEL > ./tmp/deploy.env
 $NODE_LAMBDA deploy --configFile ./tmp/deploy.env
