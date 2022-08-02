52bbc78638387ecacb35d3292caf33751d2dd856
Vova <vovadorko@gmail.com>
2022-08-02 16:10:27 +0300

    env vars

--- services/fetch-responsible-map.js
+++ services/fetch-responsible-map.js
@@ -1,11 +1,12 @@
 const AWS = require('aws-sdk');
 const { resolve } = require('url');
 
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
-});
+// is needed for local testing
+// AWS.config.update({
+//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+//   region: process.env.AWS_REGION,
+// });
 
 const S3 = new AWS.S3();
 
